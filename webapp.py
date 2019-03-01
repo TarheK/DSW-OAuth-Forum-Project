c1742b76b8a1b1ac758ed005a4e0874b054b2492
P3 <ortizadrian628@gmail.com>
2019-03-01 11:12:13 -0800

    :)

--- webapp.py
+++ webapp.py
@@ -29,20 +29,29 @@ github = oauth.remote_app(
 #TODO: Create and set a global variable for the name of you JSON file here.  The file will be storedd on Heroku, so you don't need to make it in GitHub
 info="posts.json"
 #TODO: Create the file on Heroku using os.system.  Ex) os.system("echo '[]'>"+myFile) puts '[]' into your file
-os.system("echo '[]'>"+info)
+os.system("echo []>"+info)
 @app.context_processor
 def inject_logged_in():
     return {"logged_in":('github_token' in session)}
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    with open(info,"r") as procfile:
+        data=json.load(procfile)
+    return render_template('home.html', past_posts=data)
 
 @app.route('/posted', methods=['POST'])
 def post(): 
-    return render_template('home.html')
-    With open (‘filename.json’, ‘r’) as f:
-    Data = json.load (f)
+    with open (info, 'r+') as f:
+        data = json.load (f)
+        show = request.form["message"]
+        user = session["user_data"]["login"]
+        data.append({"username": user, "message": show })
+        f.seek(0)
+        f.truncate()
+        json.dump(data,f)
+        print (data)
+    return redirect(url_for('.home'))
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post. 
 
